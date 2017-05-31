96c08d0ea5c9508100f42636fd9b868ed7600e67
Borja Lorente <blorente@ucm.es>
2017-05-31 11:51:04 +0200

    WOrks and is nonblocking

--- ghspider.py
+++ ghspider.py
@@ -6,16 +6,14 @@ import sys
 from git import Repo
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.clock import Clock
+import threading
 
 now = datetime.datetime.now()
 scriptdir = "/ghspider-%d-%d-%d/" % (now.year, now.month, now.day)
 
-if len(sys.argv) == 1:
-    print("No GitHub user specified")
-    quit()
-
-ghuser = sys.argv[1]
+ghuser = ""
+if len(sys.argv) > 1:
+    ghuser = sys.argv[1]
 
 targetpath = ""
 if len(sys.argv) < 3:
@@ -26,27 +24,32 @@ else:
 targetpath += scriptdir
 print("Saving repos from /u/%s to %s" % (ghuser, targetpath))
 
-def get_repos(params, out):
-    response = requests.get("https://api.github.com/users/%s/repos" % params['user'])
-    repolist = json.loads(response.text)
-    for repo in repolist:
-        reponame = repo["name"]
-        repourl = repo["git_url"]
-        print("Cloning repo " + reponame + " from " + repourl)
-        out.text += "Cloning repo " + reponame + " from " + repourl + '\n'
-        Clock.schedule_once(Repo.clone_from(repourl, params['path'] + reponame), 1)
-
 class GHSpiderWidget(Widget):
+    stop = threading.Event()
+
     def gather_and_backup(self, args):
         user = args['ghuser_ti'].text
         path = args['targetpath_ti'].text
         out = args['output_ti']
-        out.text = "Cloning repos of /u/%s into %s..." % (user, path)
+        out.text = "Cloning repos of /u/%s into %s...\n" % (user, path)
         repo_params = {
             'user': user,
             'path': path
         }
-        get_repos(repo_params, out)
+        threading.Thread(target=self.get_repos, args=(repo_params, out)).start()
+
+    def get_repos(self, params, out):
+        response = requests.get("https://api.github.com/users/%s/repos" % params['user'])
+        repolist = json.loads(response.text)
+        for repo in repolist:
+            if self.stop.is_set():
+                return
+            reponame = repo["name"]
+            repourl = repo["git_url"]
+            print("Cloning repo " + reponame + " from " + repourl)
+            out.text += "Cloning repo " + reponame + " from " + repourl + '...'
+            Repo.clone_from(repourl, params['path'] + reponame)
+            out.text += 'Done\n'
 
 class GHSpiderApp(App):
     def build(self):
